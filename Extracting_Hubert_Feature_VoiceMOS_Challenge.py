f9573dbc5678dcff83e548700a677910a6faa69b
Huy Nguyen <hqnguyen@jaist.ac.jp>
2022-03-15 12:43:16 +0900

    Implementation using TMGF

--- Extracting_Hubert_Feature_VoiceMOS_Challenge.py
+++ Extracting_Hubert_Feature_VoiceMOS_Challenge.py
@@ -31,7 +31,7 @@ def Extract_SSL_Feat(filepath, model, list_new,dirname, track_name):
     name = filepath [0]
     name_without_ext = name[:-4] 
     new_name =  name_without_ext +'.npy'
-    path = './data/'+track_name+'/DATA/wav/'+name  
+    path = './data/'+track_name+'/DATAtest/DATA/wav/'+name
 
     cached_path = dirname+str(new_name)
     audio_data, _ = librosa.load(path, sr=16000) 
@@ -91,13 +91,39 @@ def Save_NPY(Train_data, Val_data, track_name):
     train_data_generator(Train_data,model, track_name)
     val_data_generator(Val_data,model, track_name)
 
+def Save_NPY_Test(Test_data, track_name):
+    cp_path = './fairseq/hubert_large_ll60k.pt'
+    model, cfg, task = fairseq.checkpoint_utils.load_model_ensemble_and_task([cp_path], arg_overrides={"data": "./fairseq/data/dict"})
+    model = model[0]
+    model.eval()
+    device = torch.device("cuda:0")
+    model = model.to(device)
+
+    dirname='./data/'+track_name+'/Hubert/test/'
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+        print('Creating Directory')       
+        
+    list_new=[]
+    print('Extracting Val-SSL Features')
+    for index in range(len(Test_data)):   
+        pesq_filepath = Test_data[index].split(',')
+        list_new=Extract_SSL_Feat(pesq_filepath, model, list_new,dirname, track_name)    
+
+    with open('./data/'+track_name+'/DATAtest/DATA/sets/List_Npy_Test_hubert_MOS_Challenge_phase1_main.txt','w') as g:
+        for item in list_new:
+          g.write("%s\n" % item)
+
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('')
     parser.add_argument('--track', type=str, default='phase1-main') 
     args = parser.parse_args() 
     track_name = args.track
 
-    Train_list= ListRead('./data/'+track_name+'/DATA/sets/train_mos_list.txt')
-    Val_list= ListRead('./data/'+track_name+'/DATA/sets/val_mos_list.txt')
-    Save_NPY(Train_list, Val_list, track_name)
-	
+    # Train_list= ListRead('./data/'+track_name+'/DATA/sets/train_mos_list.txt')
+    # Val_list= ListRead('./data/'+track_name+'/DATA/sets/val_mos_list.txt')
+    # Save_NPY(Train_list, Val_list, track_name)
+    Test_list = ListRead('./data/'+track_name+'/DATAtest/DATA/sets/test_mos_list.txt')
+    Save_NPY_Test(Test_list, track_name)
